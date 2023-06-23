59622ed350b817d8f4199e11d5be9b2069ce282f
kimyounghoon <chomil96@naver.com>
2023-06-23 09:54:45 +0900

    KYH : Create Title Level

---
+++ UE_TLOZ/Source/UE_TLOZ/UE_TLOZGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_TLOZGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_TLOZ_API AUE_TLOZGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
